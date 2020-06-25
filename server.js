2990ba1cef9920cf10ca45ea5b105c19db909a0a
daaniiieel <daaniiieel@disroot.org>
2020-06-25 11:38:00 +0200

    bruh moment

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const Translate = require("./translateMoji");
+const Translate = require("moji-translate");
 const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
 
 bot.on("message", ctx => {
   ctx.reply(Translate.translate(ctx.update.message.text));
@@ -14,11 +13,11 @@ bot.on("inline_query", ctx => {
       title: "Emojify text",
       type: "article",
       id: ctx.inlineQuery.id,
-      message_text: Translate.translate(ctx.update.message.text)
+      message_text: Translate.translate(ctx.update.inline_query.query)
     }
   ];
-  console.log(Translate.translate(ctx.update.inline_query.query))
 
+  // Using context shortcut
   ctx.answerInlineQuery(result);
 });
 

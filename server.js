10496e1c1f85f6574c5e0222744369d979a1b494
Akshit Garg <garg.akshit@gmail.com>
2020-06-25 15:10:06 +0530

    Add .env support

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const Translate = require("./translateMoji");
 const Telegraf = require("telegraf");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-
-bot.on("message", ctx => {
+bot.on("message", (ctx) => {
   ctx.reply(Translate.translate(ctx.update.message.text));
 });
 
-bot.on("inline_query", ctx => {
+bot.on("inline_query", (ctx) => {
   const result = [
     {
       title: "Emojify text",
       type: "article",
       id: ctx.inlineQuery.id,
-      message_text: Translate.translate(ctx.update.message.text)
-    }
+      message_text: Translate.translate(ctx.update.message.text),
+    },
   ];
-  console.log(Translate.translate(ctx.update.inline_query.query))
+  console.log(Translate.translate(ctx.update.inline_query.query));
 
   ctx.answerInlineQuery(result);
 });

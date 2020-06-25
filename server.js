13b1bf3af0b16431b45efdbece00faeb5b0507be
Daniel Vanko <daaniiieel@disroot.org>
2020-06-25 11:34:55 +0200

    feat: initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const Translate = require("./translateMoji");
+const Telegraf = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+
+bot.on("message", ctx => {
+  ctx.reply(Translate.translate(ctx.update.message.text));
+});
+
+bot.on("inline_query", ctx => {
+  const result = [
+    {
+      title: "Emojify text",
+      type: "article",
+      id: ctx.inlineQuery.id,
+      message_text: Translate.translate(ctx.update.message.text)
+    }
+  ];
+  console.log(Translate.translate(ctx.update.inline_query.query))
+
+  ctx.answerInlineQuery(result);
+});
+
+bot.launch();
